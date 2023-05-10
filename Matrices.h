c00cabf0640310878c32b4c164fada0b449280a3
whomisjo <130593047+whomisjo@users.noreply.github.com>
2023-05-09 17:45:51 -0700

    Update Matrices.h

--- Matrices.h
+++ Matrices.h
@@ -44,25 +44,23 @@ namespace Matrices
             int cols;
     };
 
-    ///Add each corresponding element.
     ///usage:  c = a + b;
     Matrix operator+(const Matrix& a, const Matrix& b);
 
-    ///Matrix multiply.  See description.
     ///usage:  c = a * b;
     Matrix operator*(const Matrix& a, const Matrix& b);
 
-    ///Matrix comparison.  See description.
     ///usage:  a == b
     bool operator==(const Matrix& a, const Matrix& b);
 
-    ///Matrix comparison.  See description.
+
     ///usage:  a != b
     bool operator!=(const Matrix& a, const Matrix& b);
 
     ///Output matrix.
     ///Separate columns by ' ' and rows by '\n'
     ostream& operator<<(ostream& os, const Matrix& a);
+   
 
     /*******************************************************************************/
 
